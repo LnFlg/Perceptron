9a0dbd3de0f9cda4636dd1e537b04ffa7a66af3d
FraThie <40545361+FraThie@users.noreply.github.com>
2018-06-30 10:15:11 +0200

    Add files via upload

---
+++ Point.java
@@ -0,0 +1,39 @@
+
+/*
+ * Parallel Computing SS 18
+ * Perceptron Learning Algorithmen
+ * 
+ * 
+ *  
+ */
+
+public class Point {
+	private double x_value;
+	private double y_value;
+	private String color;
+		
+	
+	public Point(double x_value, double y_value, String color) {
+		super();
+		this.x_value = x_value;
+		this.y_value = y_value;
+		this.color = color;
+	}
+
+	public double getX_value() {
+		return x_value;
+	}
+	
+	public double getY_Value() {
+		return y_value;
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	
+	
+	
+	
+	
+}
