dee7b582e6b92cd6dc0403c8765eb5e3bc17b767
LnFlg <Lennard.Flegel@ruhr-uni-bochum.de>
2018-07-05 21:25:58 +0200

    testing

--- Perceptron/src/main.java
+++ Perceptron/src/main.java
@@ -14,7 +14,13 @@ public class main {
 	 * 
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double weights[] = {1,1,1};
+		PerceptronData test = new PerceptronData(weights, Double.valueOf(0));
+		ArrayList<Point> points= generateRandomSeparablePoints(10);
+		Visualizer v = Visualizer.getInstance();
+		v.showData(points, test);
+		v.setVisible(true);
+		
 	}
 	
 
